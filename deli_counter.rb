434902c327841e7b2c571f5f09a1f556fe1bf43a
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 02:11:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    intro = "The line is currently: "
+    intro = "The line is currently:"
     line_text = ""
     katz_deli.each_with_index { |name, i|
     line_text += " #{(i + 1).to_s}. #{name}"
