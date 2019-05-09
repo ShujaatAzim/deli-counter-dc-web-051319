28d8c9ef2378dacb1294771ebd919491c4bb5fc3
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 02:01:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
+  else
+    puts
   end
-
 end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number " + katz_deli[name] + " in line."
+  puts "Welcome, #{name}. You are number " + katz_deli.length + " in line."
 end
