fe41ed9fac5f828a1ada6fbc2c00f818e825eac8
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 02:07:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    put "The line is currently: "
+    print "The line is currently: "
    katz_deli.each_with_index { |name, index|
-     put "#{i + 1}. #{name} "
+     print "#{i + 1}. #{name} "
    }
   end
 end
