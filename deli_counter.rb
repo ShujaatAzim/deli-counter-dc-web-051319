49d2b4a7a2080a4f57944c0b0598053af0126110
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 02:23:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,13 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number " + katz_deli.length.to_s + " in line."
 end
+
+def now_serving
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    current_first = katz_deli.shift()
+    intro_text = "Now serving" + " #{current_first}."
+    puts intro_text
+  end
+end
