07592113884e12db64c5b4950c27f8b9ce54d453
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 01:55:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,5 +9,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli[name]} in line."
+  puts "Welcome, #{name}. You are number " + katz_deli[name] + " in line."
 end
